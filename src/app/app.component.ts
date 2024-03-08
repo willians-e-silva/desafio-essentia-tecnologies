9e86f9b726906b16495d48a447c6171effc934c6
Willians E Silva <willians.silva9620@gmail.com>
2024-03-08 13:38:25 -0300

    Created the base application, still static. Needs to be updated to comunicate with the future API.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherAirplay, featherFilePlus, featherEdit, featherXSquare, featherCheckSquare, featherPlusSquare } from '@ng-icons/feather-icons';
+import { heroUsers, heroCog6Tooth } from '@ng-icons/heroicons/outline';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  viewProviders: [provideIcons({ featherAirplay, heroUsers, featherFilePlus, featherEdit, featherXSquare, featherCheckSquare, featherPlusSquare, heroCog6Tooth })]
 })
 export class AppComponent {
   title = 'techx';
