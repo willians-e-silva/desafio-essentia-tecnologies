f752a2fbebcbaa480fbfce5a1514973fe8ee2e6b
Willians E Silva <willians.silva9620@gmail.com>
2024-03-10 20:56:39 -0300

    Front-end design finished. Added functions Add, Post, Update, and Delete for tasks. Front-end connected to the API. CSS fixed. Changed to use the tasks list as a separate module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { featherAirplay, featherFilePlus, featherEdit, featherXSquare, featherCheckSquare, featherPlusSquare } from '@ng-icons/feather-icons';
-import { heroUsers, heroCog6Tooth } from '@ng-icons/heroicons/outline';
+import { featherAirplay, featherCheckSquare, featherEdit, featherFilePlus, featherPlusSquare, featherXSquare } from '@ng-icons/feather-icons';
+import { heroCog6Tooth, heroUsers } from '@ng-icons/heroicons/outline';
+
+import { TodoComponent } from './components/todo/todo.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgIconComponent],
+  host: {ngSkipHydration: 'true'},
+  imports: [NgIconComponent, FormsModule, TodoComponent, HttpClientModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   viewProviders: [provideIcons({ featherAirplay, heroUsers, featherFilePlus, featherEdit, featherXSquare, featherCheckSquare, featherPlusSquare, heroCog6Tooth })]
